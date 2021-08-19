61ccb3a5e0452ad10c82988869b27dcb82503773
ErwanGateauMagdeleine <erwan.magdeleine@gmail.com>
2021-08-19 22:36:26 +0200

    analyzer path button is generated only once

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -102,22 +102,7 @@ void LookAndFeel::drawToggleButton(juce::Graphics& g,
 
         g.setColour(color);
 
-        auto bounds = toggleButton.getLocalBounds();
-
-        auto insetRect = bounds.reduced(4);
-
-        Path randomPath;
-
-        Random r;
-
-        randomPath.startNewSubPath(insetRect.getX(), insetRect.getY() + insetRect.getHeight() * r.nextFloat());
-
-        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
-        {
-            randomPath.lineTo(x, insetRect.getY() + insetRect.getHeight() * r.nextFloat());
-        }
-
-        g.strokePath(randomPath, PathStrokeType(1.f));
+        g.strokePath(ab->randomPath, PathStrokeType(1.f));
 
     }
 }
